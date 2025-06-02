58eb847f72f513de516bba829b4c2c6ba96f605c
graslowsnail <graslowsnail@gmail.com>
2025-06-02 13:02:30 -0400

    creating types for tik-tak-toe

--- gameLogic/game.ts
+++ gameLogic/game.ts
@@ -1,3 +1,11 @@
 // game logic for tic tac toe
 
+type Player = "x" | "o"
+type Cell = Player | null
+type Row = [Cell, Cell, Cell]
+type Board = [Row , Row, Row]
 
+
+const makeMove = (player: Player) => {
+
+}
