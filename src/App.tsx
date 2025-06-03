351d4fcfaa11ab7d2bfa554478088a3bb6baca86
graslowsnail <graslowsnail@gmail.com>
2025-06-03 10:04:00 -0400

    styling for game

--- src/App.tsx
+++ src/App.tsx
@@ -13,22 +13,23 @@ function App() {
 
   return (
   <div>
-    <div className='row'>
-        <button onClick={() => cellClick([0,0])}> {game.board[0][0]}</button>
-        <button onClick={() => cellClick([0,1])}> {game.board[0][1]}</button>
-        <button onClick={() => cellClick([0,2])}> {game.board[0][2]}</button>
+      <div className='text-5xl mb-2'>Tik Tac Toe</div>
+    <div className='w-125 grid grid-cols-3'>
+        <div className="mb-1 text-7xl content-center w-40 h-40 mr-1 bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5" onClick={() => cellClick([0,0])}> {game.board[0][0]}</div>
+        <div className=" mb-1 text-7xl content-center w-40 h-40 mr-1 bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5" onClick={() => cellClick([0,1])}> {game.board[0][1]}</div>
+        <div className=" mb -1 text-7xl content-center w-40 h-40 mr-1 bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5" onClick={() => cellClick([0,2])}> {game.board[0][2]}</div>
 
     </div>
-    <div className='row'>
-        <button onClick={() => cellClick([1,0])}> {game.board[1][0]}</button>
-        <button onClick={() => cellClick([1,1])}> {game.board[1][1]}</button>
-        <button onClick={() => cellClick([1,2])}> {game.board[1][2]}</button>
+    <div className='w-125 grid grid-cols-3'>
+        <div className="mb-1 text-7xl content-center w-40 h-40 mr-1 bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5" onClick={() => cellClick([1,0])}> {game.board[1][0]}</div>
+        <div className=" mb-1 text-7xl content-center w-40 h-40 mr-1 bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5" onClick={() => cellClick([1,1])}> {game.board[1][1]}</div>
+        <div className="mb-1 text-7xl content-center w-40 h-40 mr-1 bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5" onClick={() => cellClick([1,2])}> {game.board[1][2]}</div>
 
     </div>
-    <div className='row'>
-        <button onClick={() => cellClick([2,0])}> {game.board[2][0]}</button>
-        <button onClick={() => cellClick([2,1])}> {game.board[2][1]}</button>
-        <button onClick={() => cellClick([2,2])}> {game.board[2][2]}</button>
+    <div className='w-125 grid grid-cols-3'>
+        <div className="text-7xl content-center w-40 h-40 mr-1 bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5" onClick={() => cellClick([0,0])}> {game.board[2][0]}</div>
+        <div className=" text-7xl content-center w-40 h-40 mr-1 bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5" onClick={() => cellClick([0,1])}> {game.board[2][1]}</div>
+        <div className=" text-7xl content-center w-40 h-40 mr-1 bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5" onClick={() => cellClick([2,2])}> {game.board[2][2]}</div>
 
     </div>
       {game.gameStatus && <div>PLAYER {game.gameStatus} WON
