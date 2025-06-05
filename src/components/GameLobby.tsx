982a8ec25c268970b379690b8e9559bf54e861a7
graslowsnail <graslowsnail@gmail.com>
2025-06-05 16:29:29 -0400

    added router Link component to game list

--- src/components/GameLobby.tsx
+++ src/components/GameLobby.tsx
@@ -1,14 +1,16 @@
 // view of a game 
-import { useLoaderData } from 'react-router'
+import {Link, useLoaderData } from 'react-router'
 
 function GameLobby () {
   const { data } = useLoaderData<{data: {id: string;}[]}>()
     console.log(data)
+
+
   return (
   <div>
     <div> game list view</div>
-    <div> {data.map((containerObject, i )=> (
-        <div key={i}>{containerObject.id}</div>
+    <div className='flex flex-col'> {data.map((containerObject, i )=> (
+        <Link className='border m-2' to={`game/${containerObject.id}`} > {containerObject.id}</Link>
       ))}</div>
   </div>
   )
